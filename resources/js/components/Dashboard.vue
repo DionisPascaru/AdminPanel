9da95b807c7ec0ed66f74131c17f316e79038fb3
Dionis <denis.pascaru95@gmail.com>
2022-01-07 00:11:20 +0200

    Create Dashboar blade template, create dashboard vue component, remove home blade template

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1>Dashboard</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Dashboard',
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
