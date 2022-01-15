38342b98e13f27d84e17ab0de9f20bbefd9cef20
Dionis <denis.pascaru95@gmail.com>
2022-01-15 20:12:42 +0200

    Testing API Request after login user

--- resources/js/components/users/UsersComponent.vue
+++ resources/js/components/users/UsersComponent.vue
@@ -1,12 +1,33 @@
 <template>
-    <h1>User Component</h1>
+    <div>
+        <h1>User Component</h1>
+        <button @click="tryLogin">Login</button>
+        <button @click="loadStudents">Get students</button>
+    </div>
 </template>
 
 <script>
+import UserService from '../../services/user/user.service'
+
 export default {
     name: 'Users',
-    mounted() {
-        this.$store.dispatch("loadStudents");
+    methods: {
+        tryLogin(){
+            this.$store.dispatch('auth/login', {
+                email: 'denis.pascaru95@gmail.com',
+                password: '1234qwer'
+            });
+        },
+        loadStudents(){
+            UserService.getUserBoard().then(
+                response => {
+                    this.content = response.data;
+                },
+                error => {
+                    this.content = error.response.data.message;
+                }
+            );
+        }
     }
 }
 </script>
